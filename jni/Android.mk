d0a15075cd0714587e0fdf0b0bef75cccaa60d6d
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-06-11 08:45:54 +1000

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,51 +1,92 @@
 LOCAL_PATH := $(call my-dir)
 
+# Предварительно собранные статические библиотеки
 include $(CLEAR_VARS)
 LOCAL_MODULE    := libopenal 
 LOCAL_SRC_FILES := vendor/openal/libopenal.a
-
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := libopus 
 LOCAL_SRC_FILES := vendor/opus/libopus.a
-
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := libenet 
 LOCAL_SRC_FILES := vendor/enet/libenet.a
-
 include $(PREBUILT_STATIC_LIBRARY)
 
+# Основной модуль sampvoice
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := sampvoice
-LOCAL_LDLIBS := -llog -lOpenSLES
+LOCAL_LDLIBS := -llog -lOpenSLES -latomic
 
-LOCAL_C_INCLUDES += $(wildcard $(LOCAL_PATH)/vendor/)
+# Включаемые директории
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH) \
+    $(LOCAL_PATH)/vendor \
+    $(LOCAL_PATH)/vendor/RakNet \
+    $(LOCAL_PATH)/vendor/RakNet/SAMP \
+    $(LOCAL_PATH)/vendor/openal \
+    $(LOCAL_PATH)/vendor/opus \
+    $(LOCAL_PATH)/vendor/enet \
+    $(LOCAL_PATH)/vendor/imgui \
+    $(LOCAL_PATH)/vendor/ini
 
-# samp
-FILE_LIST := $(wildcard $(LOCAL_PATH)/*.cpp)
-FILE_LIST += $(wildcard $(LOCAL_PATH)/game/*.cpp)
-FILE_LIST += $(wildcard $(LOCAL_PATH)/clientlogic/*.cpp)
-FILE_LIST += $(wildcard $(LOCAL_PATH)/net/*.cpp)
-FILE_LIST += $(wildcard $(LOCAL_PATH)/util/*.cpp)
-FILE_LIST += $(wildcard $(LOCAL_PATH)/game/RW/RenderWare.cpp)
-FILE_LIST += $(wildcard $(LOCAL_PATH)/gui/*.cpp)
-FILE_LIST += $(wildcard $(LOCAL_PATH)/voice/*.cpp)
-FILE_LIST += $(wildcard $(LOCAL_PATH)/cryptors/*.cpp)
-FILE_LIST += $(wildcard $(LOCAL_PATH)/../santrope-tea-gtasa/encryption/*.cpp)
-FILE_LIST += $(wildcard $(LOCAL_PATH)/../santrope-tea-gtasa/encryption/*.c)
+# Список исходных файлов
+FILE_LIST := \
+    $(wildcard $(LOCAL_PATH)/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/game/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/clientlogic/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/net/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/util/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/game/RW/RenderWare.cpp) \
+    $(wildcard $(LOCAL_PATH)/gui/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/voice/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/cryptors/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/../santrope-tea-gtasa/encryption/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/../santrope-tea-gtasa/encryption/*.c)
 
-# vendor
-FILE_LIST += $(wildcard $(LOCAL_PATH)/vendor/ini/*.cpp)
-FILE_LIST += $(wildcard $(LOCAL_PATH)/vendor/RakNet/*.cpp)
-FILE_LIST += $(wildcard $(LOCAL_PATH)/vendor/RakNet/SAMP/*.cpp)
-FILE_LIST += $(wildcard $(LOCAL_PATH)/vendor/imgui/*.cpp)
-FILE_LIST += $(wildcard $(LOCAL_PATH)/vendor/hash/md5.cpp)
+# Файлы RakNet (явное указание для надежности)
+RAKNET_SRC := \
+    vendor/raknet/BitStream.cpp \
+    vendor/raknet/RakClient.cpp \
+    vendor/raknet/StringCompressor.cpp \
+    vendor/raknet/GetTime.cpp \
+    vendor/raknet/DS_Table.cpp \
+    vendor/raknet/rand.cpp \
+    vendor/raknet/DR_SHA1.cpp \
+    vendor/raknet/DR_SHA1.cpp \
+    vendor/raknet/RakNetStatistics.cpp \
+    vendor/raknet/RakNetworkFactory.cpp \
+    vendor/raknet/SAMP/SAMP.cpp \
+    vendor/raknet/SAMP/RPC.cpp
 
-LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
+# Другие vendor-файлы
+VENDOR_SRC := \
+    $(wildcard $(LOCAL_PATH)/vendor/ini/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/vendor/imgui/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/vendor/hash/*.cpp)
+
+LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%) $(RAKNET_SRC) $(VENDOR_SRC)
+
+# Флаги компиляции
+LOCAL_CPPFLAGS := \
+    -w \
+    -s \
+    -fvisibility=hidden \
+    -pthread \
+    -Wall \
+    -fpack-struct=1 \
+    -O2 \
+    -std=c++14 \
+    -fexceptions \
+    -DHAVE_CONFIG_H \
+    -D_RAKNET_SUPPORT_*
+
+# Статические библиотеки
 LOCAL_STATIC_LIBRARIES := libopenal libopus libenet
-LOCAL_CPPFLAGS := -w -s -fvisibility=hidden -pthread -Wall -fpack-struct=1 -O2 -std=c++14 -fexceptions
+
+# Сборка shared library
 include $(BUILD_SHARED_LIBRARY)
